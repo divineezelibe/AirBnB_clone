1cb4e0a361cdbb16a4b80e3f570ccda32dfe2778
kaytee07 <kofi.taylor07@gmail.com>
2023-05-16 10:01:18 +0000

    documenting our file

--- models/base_model.py
+++ models/base_model.py
@@ -10,6 +10,7 @@ from . import storage
 class BaseModel:
     """defines all common attributes/methods for other classes: """
     def __init__(self, *args, **kwargs):
+        """ this initializes our the instance of the class"""
         self.updated_at = datetime.datetime.now()
         if len(kwargs) != 0:
             for key, value in kwargs.items():
@@ -20,31 +21,27 @@ class BaseModel:
                     setattr(self, key, value)
                 else:
                     setattr(self, key, value)
-# =======
-#                     if key == "created_at" or key == "updated_at":
-#                         # Parse the string into a datetime object
-#                         datetime_obj = datetime.datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
-#                         value = datetime.datetime.isoformat(datetime_obj)
-#                         #Trying to make value a datetime object
-#                         setattr(self, key, datetime_obj)
-#                     else:
-#                         setattr(self, key, value)
-# >>>>>>> main
         else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.datetime.now()
             storage.new(self)
 
     def __str__(self):
+        """print a string representation of the current instance"""
         class_n = self.__class__.__name__
 
         return "{} {} {}".format(class_n, self.id, self.__dict__)
 
     def save(self):
+        """this function save our object in json form inside of a file"""
         self.updated_at = datetime.datetime.now()
         storage.save()
 
     def to_dict(self):
+        """
+        convert our class object to a dictionary so it can
+        be saved as json
+        """
         instance_dictionary = {}
         for key, value in self.__dict__.items():
             instance_dictionary[key] = value
